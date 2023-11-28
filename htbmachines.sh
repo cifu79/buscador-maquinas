1be09142551d79ce9a29c823e2dc26f9273dd7de
Andres Parra <cifucar79@gmail.com>
2023-11-28 11:09:17 -0500

    Anadido filtrado por SO y dificultad

--- htbmachines.sh
+++ htbmachines.sh
@@ -27,6 +27,7 @@ function helpPanel(){
     echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por un nombre de maquina${endColour}"
     echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por direccion IP${endColour}"
     echo -e "\t${purpleColour}d)${endColour}${grayColour} Buscar por la dificultad de una maquina${endColour}"
+    echo -e "\t${purpleColour}o)${endColour}${grayColour} Buscar por el sistema operativo${endColour}"
     echo -e "\t${purpleColour}y)${endColour}${grayColour} Obtener enlace de la resolucion de la maquina en youtube${endColour}"
     echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar este panel de ayuda${endColour}"
 
@@ -100,26 +101,57 @@ function getYoutubeLInk(){
 
 function getMachineDifficulty(){
     difficulty="$1"
-    results_check="$(cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep name | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+    results_check="$(cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
 
     if [ "${results_check}" ];then
         echo -e "\n${yellowColour}[+]${endColour}${grayColour} Representando las maquinas que poseen un nivel de dificultad${endColour} ${blueColour}$difficulty${endColour}${grayColour}:${endColour}\n"
-        cat bundle.js | grep "dificultad: \"Insane\"" -B 5 | grep name | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+        cat bundle.js | grep "dificultad: \"Insane\"" -B 5 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
     else
         echo -e "\n${redColour}[!] La dificultad indicada no existe${endColour}"
+    fi
+}
+
+function getOSMachines(){
+    os="$1"
+    os_results="$(cat bundle.js | grep "so: \"$os\"" -B 4 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+
+    if [ "$os_results" ];then
+        echo -e "\n${yellowColour}[+]${endColour}${grayColour} Mostrando las maquinas cuyo sistema operativo es${endColour}${blueColour} $os${endColour}${grayColour}:${endColour}\n"
+        cat bundle.js | grep "so: \"$os\"" -B 4 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+    else
+        echo -e "\n${redColour}[!] El sistema operativo indicado no existe${endColour}\n"
+    fi
+}
+
+function getOSDifficultyMachines(){
+    difficulty="$1"
+    os="$2"
+
+    check_results="$(cat bundle.js | grep "so: \"$os\"" -C 5 | grep "dificultad: \"$difficulty\"" -B 5 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+    
+    if [ "$check_results" ];then
+        echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando maquinas de dificultad${endColour} ${blueColour}$difficulty${endColour}${grayColour} que tengan sistema operativo${endColour}${purpleColour} $os${endColour}${grayColour}:${endColour}\n"
+        cat bundle.js | grep "so: \"$os\"" -C 5 | grep "dificultad: \"$difficulty\"" -B 5 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+    else
+        echo -e "\n${redColour}[!] Se ha indicado una dificultad o sistema operativo incorrectos${endColour}"
     fi
 }
 
 #indicadores
 declare -i parameter_counter=0
 
-while getopts "m:ui:y:d:h" args; do
+#chivatos
+declare -i chivato_difficulty=0
+declare -i chivato_os=0
+
+while getopts "m:ui:y:d:o:h" args; do
     case $args in
         m) machineName="$OPTARG"; let parameter_counter+=1;;
         u) let parameter_counter+=2;;
         i) ipAddress="$OPTARG"; let parameter_counter+=3;;
         y) machineName="$OPTARG"; let parameter_counter+=4;;
-        d) difficulty="$OPTARG"; let parameter_counter+=5;;
+        d) difficulty="$OPTARG";chivato_difficulty=1; let parameter_counter+=5;;
+        o) os="$OPTARG";chivato_os=1; let parameter_counter+=6;;
         h) ;;
     esac
 done
@@ -134,6 +166,10 @@ elif [ $parameter_counter -eq 4 ];then
     getYoutubeLInk $machineName
 elif [ $parameter_counter -eq 5 ];then
     getMachineDifficulty $difficulty
+elif [ $parameter_counter -eq 6 ];then
+    getOSMachines $os
+elif [ $chivato_difficulty -eq 1 ] && [ $chivato_os -eq 1 ];then
+    getOSDifficultyMachines $difficulty $os
 else
     helpPanel
 fi
